48b80c9373ecf7c581d5c1b739ef94902d28d71a
sparklyhands <sushdas3791@gmail.com>
2026-05-31 02:05:51 -0400

    implemented code using Hashset approach

--- arrays/LC217_E_ContainsDuplicate.java
+++ arrays/LC217_E_ContainsDuplicate.java
@@ -1,1 +1,24 @@
+import java.util.HashSet;
+
+public class Main {
+    public boolean containsDuplicate(int[] arr) {  
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (int n : arr) {
+            if (visited.contains(n)) {
+                return true;
+            }
+            visited.add(n);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main(); 
+        int[] arr = {2, 3, 7, 9}; 
+
+        System.out.println(main.containsDuplicate(arr));
+    }
+}
 
