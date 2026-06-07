0703a55ee6a0c4e052127bf9bab130d947a35012
sparklyhands <sushdas3791@gmail.com>
2026-06-07 10:21:56 -0400

    Update LC242_E_ValidAnagram.java
    changed logic

--- arrays/LC242_E_ValidAnagram.java
+++ arrays/LC242_E_ValidAnagram.java
@@ -1,34 +1,44 @@
-public class Main {
+import java.util.*;
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
+public class ValidAnagramSolution {
 
-        String s = "anagram";
-        String t = "nagaram";
-
-        boolean result = sol.isAnagram(s, t);
-        System.out.println("Is Anagram? " + result);
-    }
-}
-
-class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
 
+        // 1. Length check
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // 2. Frequency array
         int[] freq = new int[26];
 
+        // 3. Count characters in s
         for (char c : s.toCharArray()) {
             freq[c - 'a']++;
         }
 
+        // 4. Subtract characters in t
         for (char c : t.toCharArray()) {
             freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) {
+                return false;  // t has extra characters
+            }
         }
 
+        // 5. Final check: all counts must be zero
         for (int count : freq) {
-            if (count != 0) return false;
+            if (count != 0) {
+                return false;
+            }
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagramSolution sol = new ValidAnagramSolution();
+
+        System.out.println(sol.isAnagram("anagram", "nagaram")); // true
+        System.out.println(sol.isAnagram("rat", "car"));         // false
     }
 }
